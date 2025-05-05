bd231bf4f2769db5f44331744ef2662b1695aebe
fUn-GAmEs-rU <fUnGAmEs.rU@outlook.com>
2025-05-05 09:20:49 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.136.0/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.136.0/examples/jsm/controls/OrbitControls.js';
-import {OBJLoader} from 'https://cdn.jsdelivr.net/npm/three@0.136.0/examples/jsm/loaders/OBJLoader.js';
+import * as THREE from 'https://unpkg.com/three@0.136.0/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.136.0/examples/jsm/controls/OrbitControls.js';
+import { OBJLoader } from 'https://unpkg.com/three@0.136.0/examples/jsm/loaders/OBJLoader.js';
 
 let scene, camera, renderer, car, keys = {}, speed = 0, drift = 0, crashSound, chaosText;
 let carModel, carColor = 0xff0000;  // Initial car color
